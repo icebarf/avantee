ac6be80a01d1c2677316b5f05f5880536c5bf424
icebarf <sysgrammer@protonmail.com>
2024-01-02 16:49:58 +0530

    use the BetterSockets and icysock APIs to write the test server and client

--- testing/test-client.cpp
+++ testing/test-client.cpp
@@ -1,5 +1,8 @@
+#include "socket/error_utils.hpp"
 #include "socket/generic_sockets.hpp"
+#include "socket/sockets.hpp"
 
+#include <array>
 #include <cstdio>
 
 #ifndef ICY_ON_WINDOWS
@@ -7,92 +10,39 @@
 #define ZeroMemory(ptr, n) memset(ptr, 0, n);
 #endif
 
+#define PORT "6900"
+#define BUFLEN 512
+
 int
 main(int argc, char** argv)
 {
-  icysock::SOCKDATA data = icysock::init();
-
-  (void)data;
+  icysock::init(); // optional: only needed on windows.
   (void)argc;
-
-  struct addrinfo *result = nullptr, *ptr = nullptr;
-  struct addrinfo hints;
-  ZeroMemory(&hints, sizeof(hints));
-  hints.ai_family = AF_UNSPEC, hints.ai_socktype = SOCK_STREAM,
-  hints.ai_protocol = IPPROTO_TCP;
-
-#define PORT "6969"
-
-  long long iResult = getaddrinfo(argv[1], PORT, &hints, &result);
-  if (iResult != 0) {
-    fprintf(
-      stderr, "test: client getaddrinfo failed with code: %lld\n", iResult);
-    icysock::terminate();
-    return 1;
-  }
-
-  /* Obtain socket */
-  icysock::icy_socket sock = BAD_SOCKET;
-  ptr = result;
-  sock = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-  if (sock == BAD_SOCKET) {
-    fprintf(stderr, "test: Unable to obtain a socket\n");
-    perror("test: ");
-    freeaddrinfo(result);
-    icysock::terminate();
-    return 1;
-  }
-
-  /* connect to socket */
-  iResult = connect(sock, ptr->ai_addr, (int)ptr->ai_addrlen);
-  if (iResult == SOCK_ERR) {
-    fprintf(stderr, "test: Unable to connect to socket\n");
-    perror("test: ");
-    icysock::close_socket(sock);
-    freeaddrinfo(result);
-    icysock::terminate();
-    return 1;
-  }
-
-#define BUFLEN 512
-  int recvbuflen = BUFLEN;
+  BetterSockets::socket_hint h(BetterSockets::ip_version::IpvAny,
+                               BetterSockets::sock_kind::STREAM,
+                               BetterSockets::sock_flags::USE_HOST_IP,
+                               BetterSockets::ip_protocol::TCP);
+  BetterSockets::managed_socket sock(h, PORT, argv[1]);
+  sock.connects();
 
   const char* sendbuf = "this is a test";
-  char recvbuf[BUFLEN] = {};
+  BetterSockets::size iResult = sock.sends(sendbuf, 0);
+  printf("Bytes sent: %ld\n", iResult);
 
-  iResult = send(sock, sendbuf, (int)strlen(sendbuf), 0);
-  if (iResult == SOCK_ERR) {
-    fprintf(stderr, "test: send() failed \n");
-    perror("test: ");
-    icysock::close_socket(sock);
-    icysock::terminate();
-    return 1;
-  }
-
-  printf("Bytes sent: %lld\n", iResult);
-
-  iResult = shutdown(sock, SHUT_WR);
-  if (iResult == SOCK_ERR) {
-    fprintf(stderr, "test: shutdown() failed \n");
-    perror("test: ");
-    icysock::close_socket(sock);
-    icysock::terminate();
-    return 1;
-  }
-
+  std::array<char, BUFLEN> recvbuf{ 0 };
   do {
-    iResult = recv(sock, recvbuf, recvbuflen, 0);
+    try {
+      iResult = sock.receive(recvbuf.data(), recvbuf.size(), 0);
+    } catch (const icysock_errors::APIError& e) {
+      fprintf(stderr, "%s\n", e.what());
+      continue;
+    };
     if (iResult > 0)
-      fprintf(stdout, "Bytes receieved: %lld\n", iResult);
+      fprintf(stdout, "Bytes receieved: %ld\n", iResult);
     else if (iResult == 0)
       fprintf(stdout, "Connection closed\n");
-    else {
-      fprintf(stderr, "test: recv failed\n");
-      perror("test");
-    }
+
   } while (iResult > 0);
 
-  icysock::close_socket(sock);
-  icysock::terminate();
   return 0;
 }
--- testing/test-server.cpp
+++ testing/test-server.cpp
@@ -1,133 +1,48 @@
+#include "socket/error_utils.hpp"
 #include "socket/generic_sockets.hpp"
+#include "socket/sockets.hpp"
 
+#include <array>
 #include <cstdio>
-
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "6969"
 
 #ifndef ICY_ON_WINDOWS
 #include <string.h>
 #define ZeroMemory(ptr, n) memset(ptr, 0, n);
 #endif
 
+#define PORT "6900"
+#define BUFLEN 512
+
 int
-main(void)
+main()
 {
-  icysock::SOCKDATA wsaData = icysock::init();
-  (void)wsaData;
-  long long iResult;
+  icysock::init();
+  BetterSockets::socket_hint hint(BetterSockets::ip_version::IpvAny,
+                                  BetterSockets::sock_kind::STREAM,
+                                  BetterSockets::sock_flags::USE_HOST_IP,
+                                  BetterSockets::ip_protocol::TCP);
+  BetterSockets::managed_socket ListenSock(hint, PORT);
+  ListenSock.binds();
+  ListenSock.listens();
 
-  icysock::icy_socket ListenSocket = BAD_SOCKET;
-  icysock::icy_socket ClientSocket = BAD_SOCKET;
+  BetterSockets::managed_socket ClientSock(ListenSock.accepts());
+  BetterSockets::size iResult;
+  BetterSockets::size iSendResult;
 
-  struct addrinfo* result = NULL;
-  struct addrinfo hints;
-  ZeroMemory(&hints, sizeof(hints));
-  hints.ai_flags = AI_PASSIVE, hints.ai_family = AF_INET,
-  hints.ai_socktype = SOCK_STREAM, hints.ai_protocol = IPPROTO_TCP;
-
-  long long iSendResult;
-  char recvbuf[DEFAULT_BUFLEN];
-  int recvbuflen = DEFAULT_BUFLEN;
-
-  // Resolve the server address and port
-  iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-  if (iResult != 0) {
-    printf("getaddrinfo failed with error: %lld\n", iResult);
-
-    icysock::terminate();
-    return 1;
-  }
-
-  // Create a SOCKET for the server to listen for client connections.
-  ListenSocket =
-    socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-  if (ListenSocket == BAD_SOCKET) {
-    printf("socket failed with error\n");
-    perror("test");
-    freeaddrinfo(result);
-    icysock::terminate();
-    return 1;
-  }
-
-  // Setup the TCP listening socket
-  iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-  if (iResult == SOCK_ERR) {
-    printf("bind failed with error: %lld\n", iResult);
-    perror("test: ");
-    freeaddrinfo(result);
-    icysock::close_socket(ListenSocket);
-    icysock::terminate();
-    return 1;
-  }
-
-  freeaddrinfo(result);
-
-  iResult = listen(ListenSocket, SOMAXCONN);
-  if (iResult == SOCK_ERR) {
-    printf("listen failed with error: %lld\n", iResult);
-    perror("test: ");
-    icysock::close_socket(ListenSocket);
-    icysock::terminate();
-    return 1;
-  }
-
-  // Accept a client socket
-  ClientSocket = accept(ListenSocket, NULL, NULL);
-  if (ClientSocket == INVALID_SOCKET) {
-    printf("accept failed with error\n");
-    perror("test");
-    icysock::close_socket(ListenSocket);
-    icysock::terminate();
-    return 1;
-  }
-
-  // No longer need server socket
-  icysock::close_socket(ListenSocket);
-
+  std::array<char, BUFLEN> recvbuf = { 0 };
   // Receive until the peer shuts down the connection
   do {
 
-    iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-    recvbuf[recvbuflen - 1] = '\0';
+    iResult = ClientSock.receive(recvbuf.data(), recvbuf.size());
     if (iResult > 0) {
-      printf("Bytes received: %lld\n", iResult);
-      printf("Contents: %s\n", recvbuf);
-      // Echo the buffer back to the sender
-      iSendResult = send(ClientSocket, recvbuf, (int)iResult, 0);
-      if (iSendResult == SOCK_ERR) {
-        printf("send failed with error: %lld\n", iSendResult);
-        perror("test: ");
-        icysock::close_socket(ClientSocket);
-        icysock::terminate();
-        return 1;
-      }
-      printf("Bytes sent: %lld\n", iSendResult);
+      fprintf(stdout, "Bytes received: %ld\n", iResult);
+      fprintf(stdout, "Contents: %s\n", recvbuf.data());
+      iSendResult = ClientSock.sends(recvbuf.data());
+      fprintf(stdout, "Bytes sent: %ld\n", iSendResult);
     } else if (iResult == 0)
-      printf("Connection closing...\n");
-    else {
-      printf("recv failed with error: %lld\n", iResult);
-      perror("test: ");
-      icysock::close_socket(ClientSocket);
-      icysock::terminate();
-      return 1;
-    }
+      fprintf(stdout, "Connection closing...\n");
 
   } while (iResult > 0);
-
-  // shutdown the connection since we're done
-  iResult = shutdown(ClientSocket, SHUT_WR);
-  if (iResult == SOCK_ERR) {
-    printf("shutdown failed with error: %lld\n", iResult);
-    perror("test: ");
-    icysock::close_socket(ClientSocket);
-    icysock::terminate();
-    return 1;
-  }
-
-  // cleanup
-  icysock::close_socket(ClientSocket);
-  icysock::terminate();
 
   return 0;
 }
